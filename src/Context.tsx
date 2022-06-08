657c4b02bfe9e7ccd10a9a502a4f5562fcec8cf0
Daniil <daniil.molokov@bk.ru>
2022-06-08 23:33:36 +0300

    Основы React Context

--- src/Context.tsx
+++ src/Context.tsx
@@ -1,10 +1,17 @@
-import { ReactElement } from 'react';
+import { createContext, ReactElement, useContext } from 'react';
 
 type Theme = 'light' | 'dark';
 
-export function ThemeProvider(props: {
-    theme: Theme;
-    children: ReactElement;
-}) {}
+export function ThemeProvider(props: { theme: Theme; children: ReactElement }) {
+    return (
+        <ThemeContext.Provider value={props.theme}>
+            {props.children}
+        </ThemeContext.Provider>
+    );
+}
 
-export function useTheme(): Theme {}
+const ThemeContext = createContext<Theme>('light');
+
+export function useTheme(): Theme {
+    return useContext(ThemeContext);
+}
